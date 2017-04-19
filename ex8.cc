8d31ccb7c28884b54b718ea34a670ea0c57ac389
Zhixing Wang <wangz58@cs.washington.edu>
2017-04-19 09:09:27 -0700

    finished redo style

--- ex8.cc
+++ ex8.cc
@@ -4,20 +4,19 @@
 // wangz58@cs.washington.edu
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-	int n = -1;
-	std::cout << "Type in a positive integer to factor";
-	std::cin >> n;
-	if (n < 0) {
-		std::cout << "Please type in a positive integer";
-		return 1;
-	}
-	for (int i = 1; i < n; i++) {
-		if (n % i == 0) {
-			std::cout << i << " ";
-		}
-	}
-	std::cout << n << std::endl;
-	return 0;
+int main(int argc, char const *argv[]) {
+  int n = -1;
+  std::cout << "Type in a positive integer to factor";
+  std::cin >> n;
+  if (n < 0) {
+    std::cout << "Please type in a positive integer";
+    return 1;
+  }
+  for (int i = 1; i < n; i++) {
+    if (n % i == 0) {
+      std::cout << i << " ";
+    }
+  }
+  std::cout << n << std::endl;
+  return 0;
 }
