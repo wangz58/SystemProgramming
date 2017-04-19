24d9f084e88138ed3687777565b66ed5408ab5b7
Zhixing Wang <wangz58@cs.washington.edu>
2017-04-19 09:02:57 -0700

    added ex8

---
+++ ex8.cc
@@ -0,0 +1,23 @@
+// Copyright [2017] <wangz58@uw>
+// Zhixing Wang
+// 1333101
+// wangz58@cs.washington.edu
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int n = -1;
+	std::cout << "Type in a positive integer to factor";
+	std::cin >> n;
+	if (n < 0) {
+		std::cout << "Please type in a positive integer";
+		return 1;
+	}
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) {
+			std::cout << i << " ";
+		}
+	}
+	std::cout << n << std::endl;
+	return 0;
+}
