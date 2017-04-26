618c4994199dcb2cf9a1e63b3ce0aa9e57799edb
Zhixing Wang <wangz58@cs.washington.edu>
2017-04-26 09:33:55 -0700

    finished ex11

--- ex11/Vector.cc
+++ ex11/Vector.cc
@@ -18,7 +18,9 @@ Vector::Vector(const double x, const double y, const double z) {
   values_[2] = z;
 }
 
-Vector::Vector(const Vector &v) : Vector(v.values_[0], v.values_[1], v.values_[2]) {
+Vector::Vector(const Vector &v) : Vector(v.values_[0],
+                                         v.values_[1],
+                                         v.values_[2]) {
 }
 
 Vector::~Vector() {
@@ -27,23 +29,31 @@ Vector::~Vector() {
 
 // addition: return element-wise sum of this + other
 Vector Vector::operator+(const Vector &other) const {
-  return Vector(values_[0] + other.values_[0], values_[1] + other.values_[1], values_[2] + other.values_[2]);
+  return Vector(values_[0] + other.values_[0],
+                values_[1] + other.values_[1],
+                values_[2] + other.values_[2]);
 }
 
 // subtraction: return element-wise difference of this - other
 Vector Vector::operator-(const Vector &other) const {
-  return Vector(values_[0] - other.values_[0], values_[1] - other.values_[1], values_[2] - other.values_[2]);
+  return Vector(values_[0] - other.values_[0],
+                values_[1] - other.values_[1],
+                values_[2] - other.values_[2]);
 }
 
 // dot-product: if this is (a,b,c) and other is (x,y,z),
 // return ax+by+cz
 double Vector::operator*(const Vector &other) const {
-  return values_[0]*other.values_[0] + values_[1]*other.values_[1] + values_[2]*other.values_[2];
+  return values_[0]*other.values_[0] +
+         values_[1]*other.values_[1] +
+         values_[2]*other.values_[2];
 }
 
 // scalar multiplication: if this is (a,b,c), return (ak,bk,ck)
 Vector Vector::operator*(const double k) const {
-  return Vector(values_[0] * k, values_[1] * k, values_[2] * k);
+  return Vector(values_[0] * k,
+         values_[1] * k,
+         values_[2] * k);
 }
 
 // Vector assignment
@@ -64,7 +74,9 @@ Vector &Vector::operator=(const Vector &rhs) {
 
 // Stream output: << for Vectors
 std::ostream & operator<<(std::ostream &out, const Vector &v) {
-  out << "(" << v.values_[0] << "," << v.values_[1] << "," << v.values_[2] << ")";
+  out << "(" << v.values_[0]
+      << "," << v.values_[1]
+      << "," << v.values_[2] << ")";
   return out;
 }
 }
